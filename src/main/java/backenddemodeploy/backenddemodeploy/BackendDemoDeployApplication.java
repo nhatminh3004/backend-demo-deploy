a3c097b0db1267974dda73f2022185a5a8f1423a
nhatminh3004 <nhatminhminh57@gmail.com>
2023-02-07 21:40:03 +0700

    Initial commit

---
+++ src/main/java/backenddemodeploy/backenddemodeploy/BackendDemoDeployApplication.java
@@ -0,0 +1,13 @@
+package backenddemodeploy.backenddemodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDemoDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDemoDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backenddemodeploy/backenddemodeploy/BackendDemoDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package backenddemodeploy.backenddemodeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDemoDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
